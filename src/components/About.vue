2d01f7560737f888e3a674a2a8cd90deade505ff
mel <melgriffin@protonmail.com>
2018-07-13 20:20:42 -0400

    Add about page

--- src/components/About.vue
+++ src/components/About.vue
@@ -4,7 +4,10 @@
     <div class="container">
         <h1 style="margin-top:5rem" class="is-size-2">About Learn Steno</h1>
         <p>
-            TODO: write about page
+          Learn Steno was developed by Mel Griffin and Ian Mcxa. It is open source and available on <a href="https://github.com/mellonite/learnsteno">github</a>.
+        </p>
+        <p>
+          The lessons and drills were written by Zack Brown, and can be found at <a href="https://sites.google.com/site/ploverdoc/home">Learn Plover!</a>
         </p>
     </div>
 </div>
