d39dbde0b3fcaa34dfe672b055053a5acdd831d3
Ian Mcxa <ianmcxa@tutanota.com>
2018-07-14 23:50:37 -0400

    Transcription view is working

--- src/components/Transcription.vue
+++ src/components/Transcription.vue
@@ -6,9 +6,9 @@
         <progress class="progress is-primary" :value="progress" max="1"></progress>
         <!-- only render the drill if there are still words left -->
         <template v-if="sentenceIndex < transcription.sentences.length">
-            <button class="button is-info is-fullwidth" :click="start()">Start</button>
-            <div id="input" contenteditable="true" role="textbox" v-on:input="inputUpdate()"
-            style="margin-top:1rem" class="textarea is-primary" v-html="input"></div>
+            <button class="button is-info is-fullwidth" v-on:click="playAudio">{{ startTime == null ? 'Start' : 'Repeat Audio' }}</button>
+            <p style="min-height: 1.5rem; margin: .5rem" v-html="inputRender"></p>
+            <textarea id="input" v-model="input" style="margin-top:1rem" class="textarea is-primary"></textarea>
             <!-- Options card -->
             <div class="card" style="width:35rem;margin-top:2rem">
                 <div class="card-content">
@@ -30,12 +30,6 @@
                   <div>
                       <p class="heading">Words per minute</p>
                       <p class="title">{{ wordsPerMinute }}</p>
-                  </div>
-              </div>
-              <div class="level-item has-text-centered">
-                  <div>
-                      <p class="heading">Missed strokes</p>
-                      <p class="title">{{ overallErrors }}</p>
                   </div>
               </div>
               <div class="level-item has-text-centered">
@@ -77,6 +71,15 @@ export default {
         }
         // replace paths with audio and return
         return sentences.map((s) => { return [new Audio(require('../assets/audio/transcription/' + s[0] + '.ogg')), s[1]]})
+    },
+    inputRender () {
+        let correctArray = this.currentSentence[1].toUpperCase().replace(/[^a-zA-Z ]/, "").split(' ')
+        let inputArray = this.input.split(' ')
+        return inputArray.map((word, index) => {
+            if (word.toUpperCase().replace(/[^a-zA-Z ]/, "") == correctArray[index])
+                return word
+            else return '<span style="background-color: hsl(348, 100%, 90%)">' + word + '</span>'
+        }).join(' ')
     }
   },
   created () {
@@ -86,10 +89,8 @@ export default {
       return {
         // please read this: https://vuejs-templates.github.io/webpack/static.html
         transcription: require('../assets/practicedata/transcription/' + this.transcrptionName + '.json'),
-        input: '',
         sentenceIndex: 0,
-        wordErrors: 0,
-        overallErrors: 0,
+        input: '',
         startTime: null,
         endTime: null,
         showLayout: false,
@@ -97,11 +98,26 @@ export default {
         correctSound: new Audio(require('../assets/audio/correct.ogg'))
       }
   },
+  watch: {
+    input () {
+        if (this.isInputCorrect(this.input)) {
+            this.sentenceIndex++
+            this.input = ''
+            this.correctSound.play()
+            if (this.sentences.length == this.sentenceIndex) {
+                this.endTime = new Date()
+                //TODO save data
+            } else {
+                this.playAudio()
+            }
+        }
+    },
+  },
   methods: {
-      start () {
+      playAudio () {
         this.startTime = new Date()
         //TODO play first recording
-        this.sentences[this.sentenceIndex][0].play
+        this.sentences[this.sentenceIndex][0].play()
       },
       formatDuration(timeInMS) {
           let minutes = Math.floor(timeInMS / 1000 / 60)
@@ -109,17 +125,13 @@ export default {
           if (seconds < 10) seconds += '0'
           return minutes + ':' + seconds
       },
-      inputUpdate () {
-          //hack because v-html doesn't update which html changes
-          /*let input = document.getElementById('input').innerHTML
-          let inputArray = input.split(' ').map((w) => { return w.replace('<span>', '').replace('</span>', '') })
-          let output = inputArray.map((w) => {
-            return w === 'incorrect' ? '<span style="background-color: red">' + w + '</span>' : w
-          }).join(' ')
-          console.log(input)
-          console.log(inputArray)
-          console.log(output)
-          this.input = input*/
+      isInputCorrect(input) {
+          // a little premature optimization, return false if we don't have the same number of words
+          if (this.currentSentence[1].split(' ').length !== input.split(' ').length) return false
+
+          let correct = this.currentSentence[1].toUpperCase().replace(/[^a-zA-Z ]/, "")
+          let attempt = input.trim().toUpperCase().replace(/[^a-zA-Z ]/, "")
+          return attempt === correct
       }
   },
   components: {
