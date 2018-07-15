2165e43845100fafcefe2c7bfaddbd294158011b
mel <melgriffin@protonmail.com>
2018-07-15 00:14:44 -0400

    Update lessons design

--- src/components/lessons/Lesson1a.vue
+++ src/components/lessons/Lesson1a.vue
@@ -1,54 +1,63 @@
 <template>
 <div>
     <navbar page='lessons'></navbar>
-    <div class="container content lesson">
-      <div class="box">
-        <h1 class="is-size-2">Lesson 1: Fingers and Keys</h1>
-        <p class="parmar">
+    <div class="container lesson">
+      <div class="columns">
+        <div class="column">
+          <div class="box content">
             <h2>The Keyboard</h2>
-            A nice place to start learning Plover is with its most obvious feature - the keyboard.
-        </p>
-        <p class="parmar">
-            What is this thing? Some letters seem to be missing, others seem to be duplicated. It looks weird.
-            <img class="image" src="../../assets/img/Fig1.svg">
-        </p>
-        <p class="parmar">
-            The most immediately useful thing to know is that all the consonant keys map to their corresponding consonant sounds.
-            Plover - and all steno systems - express words primarily as groups of sounds rather than groups of letters of the alphabet.
-            So right away you now know that either of the two <span class="stroke">P</span> keys would be used to type the 'p' sound, either of the two <span class="stroke">R</span> keys would be used to type the 'r' sound,
-            and so on. That's already a lot of consonant sounds available for typing, without having to memorize anything.
-            The current lesson will focus on these consonants and the four somewhat more complicated vowel keys; the rest will be covered in later lessons.
-        </p>
-        <p class="parmar">
-            Before anything else, you should learn the proper keyboard fingering. If you've ever studied touch-typing on the QWERTY keyboard,
-            you probably spent quite a bit of time getting comfortable with the home position and with the proper ways to stretch your fingers
-            to reach all the keys. On a steno keyboard, none of that complexity exists, and you can learn proper fingering in about one minute.
-        </p>
-        <p class="parmar">
-            In the following diagram, the blue boxes contain the keys associated with each finger, and the tan circles represent the home position
-             for the finger responsible for the keys in that blue box.
-        </p>
-        <img class="image" src="../../assets/img/Fig2.svg">
-        <p class="parmar">
-            As you can see, with the exception of the left little finger, each finger rests at the border between two keys; and that finger is used
-            to press either (or both) of those keys. The right little finger has the additional responsibility of the <span class="stroke">D</span>
-            and <span class="stroke">Z</span> keys, and the right
-            forefinger is responsible for the asterisk. The thumbs rest on their sides, rather than on their pads. But you can easily see that unlike
-            QWERTY keyboards, none of your fingers will ever move very far from their home positions. This helps with the blazingly fast typing speeds.
-        </p>
+            <p class="parmar">
+                A nice place to start learning Plover is with its most obvious feature - the keyboard.
+            </p>
+            <p class="parmar">
+                What is this thing? Some letters seem to be missing, others seem to be duplicated. It looks weird.
+                <img class="image" src="../../assets/img/Fig1.svg">
+            </p>
+            <p class="parmar">
+                The most immediately useful thing to know is that all the consonant keys map to their corresponding consonant sounds.
+                Plover - and all steno systems - express words primarily as groups of sounds rather than groups of letters of the alphabet.
+                So right away you now know that either of the two <span class="stroke">P</span> keys would be used to type the 'p' sound, either of the two <span class="stroke">R</span> keys would be used to type the 'r' sound,
+                and so on. That's already a lot of consonant sounds available for typing, without having to memorize anything.
+                The current lesson will focus on these consonants and the four somewhat more complicated vowel keys; the rest will be covered in later lessons.
+            </p>
+            <p class="parmar">
+                Before anything else, you should learn the proper keyboard fingering. If you've ever studied touch-typing on the QWERTY keyboard,
+                you probably spent quite a bit of time getting comfortable with the home position and with the proper ways to stretch your fingers
+                to reach all the keys. On a steno keyboard, none of that complexity exists, and you can learn proper fingering in about one minute.
+            </p>
+            <p class="parmar">
+                In the following diagram, the blue boxes contain the keys associated with each finger, and the tan circles represent the home position
+                 for the finger responsible for the keys in that blue box.
+            </p>
+            <img class="image" src="../../assets/img/Fig2.svg">
+            <p class="parmar">
+                As you can see, with the exception of the left little finger, each finger rests at the border between two keys; and that finger is used
+                to press either (or both) of those keys. The right little finger has the additional responsibility of the <span class="stroke">D</span>
+                and <span class="stroke">Z</span> keys, and the right
+                forefinger is responsible for the asterisk. The thumbs rest on their sides, rather than on their pads. But you can easily see that unlike
+                QWERTY keyboards, none of your fingers will ever move very far from their home positions. This helps with the blazingly fast typing speeds.
+            </p>
+          </div>
+          <router-link to="/lesson/1b" class="button is-primary is-pulled-right">Next</router-link>
+        </div>
+        <div class="column is-one-third">
+          <lessonmenu></lessonmenu>
+        </div>
+
       </div>
-      <router-link to="/lesson/1b" class="button is-primary is-pulled-right" style="margin-top:1rem">Next</router-link>
     </div>
 </div>
 </template>
 
 <script>
 import NavBar from '../Navbar.vue'
+import Lesson1menu from './Lesson1menu.vue'
 
 export default {
   name: 'Lesson1a',
   components: {
-      'navbar': NavBar
+      'navbar': NavBar,
+      'lessonmenu': Lesson1menu,
   }
 }
 </script>
---
+++ src/components/lessons/Lesson1menu.vue
@@ -0,0 +1,30 @@
+<template>
+<div class="tile is-ancestor">
+  <div class="tile is-parent">
+    <article class="tile is-child box">
+      <div>
+        <p class="title">Lesson 1</p>
+        <p class="subtitle">Fingers and Keys</p>
+        <aside class="menu">
+          <ul class="menu-list">
+            <li>
+              <router-link to="/lesson/1a">The Keyboard</router-link>
+              <ul>
+                <li><router-link to="/lesson/1b">Rules For Short Vowel Sounds</router-link></li>
+                <li><router-link to="/lesson/1b">What About Long Vowels and Diphthongs?</router-link></li>
+              </ul>
+            </li>
+            <li><router-link to="/lesson/1c">Stroking Your First Words</router-link></li>
+          </ul>
+        </aside>
+      </div>
+    </article>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Lesson1menu',
+}
+</script>
