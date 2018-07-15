e3e9c405dd5ea7b4e1d055f68fa7d44ab1f2e7a2
mel <melgriffin@protonmail.com>
2018-07-15 09:39:30 -0400

    Replace Lorem Ipsum with placeholder score

--- src/components/Practice.vue
+++ src/components/Practice.vue
@@ -45,10 +45,10 @@
             <div class="tile is-parent">
               <article class="tile is-child box">
                 <div class="content">
-                  <p class="title">Daily Goal</p>
+                  <p class="title">Score</p>
+                  <p class="subtitle">Placeholder</p>
                   <div class="content">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam semper diam at erat pulvinar, at pulvinar felis blandit. Vestibulum volutpat tellus diam, consequat gravida libero rhoncus ut. Morbi maximus, leo sit amet vehicula eleifend, nunc dui porta orci, quis semper odio felis ut quam.</p>
-                    <p>Integer sollicitudin, tortor a mattis commodo, velit urna rhoncus erat, vitae congue lectus dolor consequat libero. Donec leo ligula, maximus et pellentesque sed, gravida a metus. Cras ullamcorper a nunc ac porta. Aliquam ut aliquet lacus, quis faucibus libero. Quisque non semper leo.</p>
+                    <h1>9001</h1>
                   </div>
                 </div>
               </article>
