75d7b7d87645e1ef192d202207d445cde0b88e60
Ian Mcxa <ianmcxa@tutanota.com>
2018-07-15 20:23:08 -0400

    Cleaned up unsupported browser

--- src/components/UnsupportedBanner.vue
+++ src/components/UnsupportedBanner.vue
@@ -1,9 +1,7 @@
 <template>
-  <section v-if="showUnsupported" class="hero is-danger is-small">
-    <div class="hero-body">
-      <div class="container narrow" style="margin-top:3em">
+    <b-notification v-if="!canPlayOgg" style="margin-top:3rem" type="is-danger">
+      <div class="container narrow">
         <h1 class="title">
-          <button style="float: right" class="delete is-large" v-on:click="showUnsupported = false"></button>
           <font-awesome-icon icon="exclamation-triangle"></font-awesome-icon>
           Unsupported Browser
         </h1>
@@ -11,18 +9,12 @@
             You are using an unsupported browser. You can still use learnsteno, but certain features may not work.
         </h2>
       </div>
-    </div>
-  </section>
+    </b-notification>
 </template>
 
 <script>
 export default {
   name: 'UnsupportedBanner',
-  data () {
-      return {
-          showUnsupported: !this.canPlayOgg()
-      }
-  },
   methods: {
       canPlayOgg () {
           let obj = document.createElement('video')
